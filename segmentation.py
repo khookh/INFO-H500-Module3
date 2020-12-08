57dbf9f191e172d522cbbbcb79111c909073e75d
sdonne <stefanodonne@gmail.com>
2020-12-08 17:19:47 +0100

    Adds optimal threshold research

--- segmentation.py
+++ segmentation.py
@@ -9,50 +9,50 @@ from skimage import img_as_ubyte
 from skimage.io import imread
 
 
+# return the normalized np array of the histogram
 def norm_hist(ima):
     histogram, bin_edges = np.histogram(
-        ima.flatten(), bins=256, range=(15, 256)
+        # histogram range changed to remove 'garbage' low light values
+        ima.flatten(), bins=256, range=(25, 256)
     )
     return 1. * histogram / np.sum(histogram)
 
 
-def display_hist(ima, vmin=None, vmax=None):
-    plt.figure(figsize=[10, 5])
-    nh = norm_hist(ima)
-    plt.subplot(1, 2, 1)
-    plt.imshow(ima, cmap=cm.gray, vmin=vmin, vmax=vmax)
-    plt.subplot(1, 2, 2)
-    plt.plot(nh, label='hist.')
-    plt.legend()
-    plt.xlabel('gray level')
-    plt.show()
+# find optimal threshold in an histogram h from an initial threshold t
+# (taken from Module 3 correction)
+def optimal_threshold(h, t):
+    # Cut distribution in 2
+    g1 = h[:t]
+    g2 = h[t:]
+    # Compute the centroids
+    m1 = (g1 * np.arange(0, t)).sum() / g1.sum()
+    m2 = (g2 * np.arange(t, len(h))).sum() / g2.sum()
+    # Compute the new threshold
+    t2 = int((m1 + m2) / 2)
+    if t2 != t:
+        return optimal_threshold(h, t2)
+    return t2
 
 
+# print on the standard output the measured tumor area
 def _score(img):
     detected = np.nonzero(img)
     # every pixel corresponds to 0.013225 cm² of tumor area
     score = 0.013225 * np.size(detected)
-    print("The measured area of the tumor is of approximately %.2f cm²" % score)
+    print("The measured area of the tumor is approximately of %.2f cm²" % score)
 
 
 mri = img_as_ubyte(imread(str(sys.argv[1]), as_gray=True))
-mode = str(sys.argv[2])
-display_hist(mri)
 
-# apply basic segmentation based on gray level
-# then refine the result with morphological transforms
-# very fast operation, is good for approximating
-if mode == "seg_morph":
-    seg = mri > 175  # threshold value based on observation
-    seg = cv.morphologyEx(seg.astype('uint8'), cv.MORPH_CLOSE, np.ones((9, 9), np.uint8))  # close areas
-    seg = cv.morphologyEx(seg.astype('uint8'), cv.MORPH_OPEN, np.ones((13, 13), np.uint8))  # remove small/non-desired
-    # shows the original MRI with the detected tumour highlighted on it
-    plt.figure()
-    plt.imshow(mri, cmap=cm.gray)
-    plt.imshow(seg, alpha=0.3)
-    plt.savefig('mri_segMorph.png')
-    _score(seg)
-
-
-else:
-    print("watershed mode")
+# a basic gray value segmentation is made based on the optimal threshold
+t = optimal_threshold(norm_hist(mri), 150)
+seg = mri > t + 25  # threshold value is shifted to take into account initial low value removal
+# morphological transforms are used to refine the result
+seg = cv.morphologyEx(seg.astype('uint8'), cv.MORPH_CLOSE, np.ones((9, 9), np.uint8))  # close areas
+seg = cv.morphologyEx(seg.astype('uint8'), cv.MORPH_OPEN, np.ones((13, 13), np.uint8))  # remove small/non-desired
+# shows the original MRI with the detected tumour highlighted on it
+plt.figure()
+plt.imshow(mri, cmap=cm.gray)
+plt.imshow(seg, alpha=0.3)
+plt.savefig('mri_segMorph.png')
+_score(seg)
